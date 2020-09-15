112e4686b7a3a68490e7a363a0cd4eecece80aad
wangyazhen <603684240@qq.com>
2020-09-15 18:53:17 +0800

    reconsitution comp

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'gatsby'
 import styles from './header.module.css'
 import Logo from './header_logo.png'
@@ -6,60 +6,45 @@ import Music from './music'
 import TongJi from './tongji'
 import config from '../../utils/config'
 
+const isMobile = () => typeof window === 'object' ? window.innerWidth < 768 : false
 
-class Header extends React.PureComponent {
-  state = {
-    menuActive: false,
-    isMobile: false,
-  }
-  componentDidMount() {
-    this.setState({ isMobile: window.screen.width < 768})
-  }
-
-  change = () => {
-    this.setState({menuActive: !this.state.menuActive})
-  }
-  render() {
-    const props = {
-      menuActive: this.state.menuActive,
-      isMobile: this.state.isMobile,
-      change: this.change,
-    }
-    return <HeaderComponent {...props} />
-  }
-}
-
-export default Header
-
-
-const HeaderComponent = ({ menuActive,  isMobile, change}) => (
-  <header className={styles.header}>
-    <div>
-      <Music />
-      <TongJi />
-      <div className={styles.imglogo}>
-        <Link to="/"><img style={{borderRadius: '50%'}} src={Logo} alt={config.title} title={config.title} /></Link>
-      </div>
-      <div className={styles.textlogo}>
-        <h1 className="site-name"><Link to="/">{config.title}</Link></h1>
-        <h2 className="blog-motto">{config.subtitle}</h2>
-      </div>
-      { isMobile &&
-        <div className="navbar header-navbar" style={{backgroundColor: menuActive ? '#e05f5f' : '#b7b6b6'}} onClick={change}>
-          <span className="navbutton navmobile" title="菜单">菜单</span>
+export default function Header() {
+  const [menuActive, setMenuActive] = useState(false)
+  
+  return (
+    <header className={styles.header}>
+      <div>
+        <Music />
+        <TongJi />
+        <div className={styles.imglogo}>
+          <Link to="/"><img style={{borderRadius: '50%'}} src={Logo} alt={config.title} title={config.title} /></Link>
         </div>
-      }
-      <nav className={styles.nav} style={{display: isMobile && !menuActive ? 'none' : 'block'}}>
-        <ul>
-          {config.menuNavs.map(item => <li key={item.link}><Link to={item.link}>{item.title}</Link></li>)}          
-          <li className="search-li-item" style={{display: isMobile ? 'none' : 'block'}}>
-            <form className="search" action="//google.com/search" method="get" acceptCharset="utf-8">
-              <input type="text" className={styles.searchInput} name="q" autoComplete="off" maxLength={20} placeholder="搜索" />
-              <input type="hidden" name="q" defaultValue="site:yazhen.me" />
-            </form>
-          </li>
-        </ul>
-      </nav>			
-    </div>
-  </header>
-)
+        <div className={styles.textlogo}>
+          <h1 className="site-name"><Link to="/">{config.title}</Link></h1>
+          <h2 className="blog-motto">{config.subtitle}</h2>
+        </div>
+        {
+          isMobile() &&
+          <div
+            className="navbar header-navbar"
+            style={{backgroundColor: menuActive ? '#e05f5f' : '#b7b6b6'}}
+            onClick={() => setMenuActive(!menuActive)}
+          >
+            <span className="navbutton navmobile" title="菜单">菜单</span>
+          </div>
+        }
+        <nav className={styles.nav} style={{display: isMobile() && !menuActive ? 'none' : 'block'}}>
+          <ul>
+            {config.menuNavs.map(item => <li key={item.link}><Link to={item.link}>{item.title}</Link></li>)}
+            <li className="search-li-item" style={{display: isMobile() ? 'none' : 'block'}}>
+              <form className="search" action="//google.com/search" method="get" acceptCharset="utf-8">
+                <input type="text" className={styles.searchInput} name="q" autoComplete="off" maxLength={20} placeholder="搜索" />
+                <input type="hidden" name="q" defaultValue="site:yazhen.me" />
+              </form>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
--- src/components/layout/siderBar.js
+++ src/components/layout/siderBar.js
@@ -2,58 +2,56 @@ import React from 'react'
 import { Bookmark, Profile, HotCard } from './side'
 
 
-class SideBar extends React.Component {
-  render() {
-    return (
-      <div id="side-bar">
+function SideBar() {
+  return (
+    <div id="side-bar">
 
-        <Profile title="我的名片" />
-        <Bookmark
-          title="书签"
-          links={[
-            { text: 'Web前端技能', title: '', link: 'https://www.qdskill.com' },
-            { text: 'WiKiHow', title: '值得每日一看', link: 'https://zh.wikihow.com/%E9%A6%96%E9%A1%B5' },
-            { text: '设计导航', title: '', link: 'http://hao.shejidaren.com/' },
-            { text: '凹凸实验室', title: '', link: 'https://aotu.io/' },
-            { text: '大咖说', title: '', link: 'http://www.itdks.com/' },
-            { text: '网讯博客', title: '', link: 'http://www.wonxun.net/' },
-            { text: '问答库', title: '', link: 'https://www.asklib.com/' },
-            { text: '如何成功[英]', title: '英文', link: 'http://blog.samaltman.com/how-to-be-successful' },
-            { text: '程序变量取名', title: '取名', link: 'https://unbug.github.io/codelf/' },
-            { text: '免费商用字体', title: '字体', link: 'https://www.100font.com/' },
-            { text: '大前端-网址导航', title: '大前端', link: 'http://www.daqianduan.com/nav' },
-          ]}
-        />
+      <Profile title="我的名片" />
+      <Bookmark
+        title="书签"
+        links={[
+          { text: 'Web前端技能', title: '', link: 'https://www.qdskill.com' },
+          { text: 'WiKiHow', title: '值得每日一看', link: 'https://zh.wikihow.com/%E9%A6%96%E9%A1%B5' },
+          { text: '设计导航', title: '', link: 'http://hao.shejidaren.com/' },
+          { text: '凹凸实验室', title: '', link: 'https://aotu.io/' },
+          { text: '大咖说', title: '', link: 'http://www.itdks.com/' },
+          { text: '网讯博客', title: '', link: 'http://www.wonxun.net/' },
+          { text: '问答库', title: '', link: 'https://www.asklib.com/' },
+          { text: '如何成功[英]', title: '英文', link: 'http://blog.samaltman.com/how-to-be-successful' },
+          { text: '程序变量取名', title: '取名', link: 'https://unbug.github.io/codelf/' },
+          { text: '免费商用字体', title: '字体', link: 'https://www.100font.com/' },
+          { text: '大前端-网址导航', title: '大前端', link: 'http://www.daqianduan.com/nav' },
+        ]}
+      />
 
-        <Bookmark 
-          title="学习"
-          links={[
-            { text: '免费书籍', title: 'GitHub上的书籍', link: 'https://github.com/EbookFoundation/free-programming-books/blob/master/free-programming-books-zh.md' },
-            { text: 'Best of Js', title: 'Best of Js ', link: 'https://bestofjs.org/' },
-            { text: '33个概念', title: 'JavaScript开发者应懂的33个概念 ', link: 'https://github.com/stephentian/33-js-concepts' },
-            {
-              text: '30 seconds of code', 
-              title: 'Curated collection of useful JavaScript snippets that you can understand in 30 seconds or less.', 
-              link: 'https://30secondsofcode.org/',
-            },
-          ]}
-        />
+      <Bookmark
+        title="学习"
+        links={[
+          { text: '免费书籍', title: 'GitHub上的书籍', link: 'https://github.com/EbookFoundation/free-programming-books/blob/master/free-programming-books-zh.md' },
+          { text: 'Best of Js', title: 'Best of Js ', link: 'https://bestofjs.org/' },
+          { text: '33个概念', title: 'JavaScript开发者应懂的33个概念 ', link: 'https://github.com/stephentian/33-js-concepts' },
+          {
+            text: '30 seconds of code',
+            title: 'Curated collection of useful JavaScript snippets that you can understand in 30 seconds or less.',
+            link: 'https://30secondsofcode.org/',
+          },
+        ]}
+      />
 
-        <HotCard />
+      <HotCard />
 
-        <Bookmark
-          title="友情链接"
-          links={[
-            { text: '博客列表', title: '中文独立博客列表', link: 'https://github.com/timqian/chinese-independent-blogs' },
-            { text: 'Surmon', title: '司马萌', link: 'https://surmon.me/' },
-            { text: '老D博客', title: '老D博客', link: 'https://laod.cn/' },
-            { text: 'LFhacks', title: 'LFhacks', link: 'https://www.lfhacks.com/' },
-          ]}
-        />
-        
-      </div>
-    )
-  }
+      <Bookmark
+        title="友情链接"
+        links={[
+          { text: '博客列表', title: '中文独立博客列表', link: 'https://github.com/timqian/chinese-independent-blogs' },
+          { text: 'Surmon', title: '司马萌', link: 'https://surmon.me/' },
+          { text: '老D博客', title: '老D博客', link: 'https://laod.cn/' },
+          { text: 'LFhacks', title: 'LFhacks', link: 'https://www.lfhacks.com/' },
+        ]}
+      />
+
+    </div>
+  )
 }
 
 export default SideBar
