76d99f408c7b4f24b5fcd8ec561f0587094bd2e1
wangyazhen <603684240@qq.com>
2020-09-22 17:27:03 +0800

    support typescript :clap::clap:

--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export default function () {
-    return <div>
-        我是首页
-    </div>
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { NextPage } from 'next'
+
+const IndexPage: NextPage = () => {
+    return <div>
+        我是首页, 朕
+    </div>
+}
+
+export default IndexPage
